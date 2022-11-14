4e73df1529ace5c8f344ef3ed909a786e838c9b6
Javier Grillone <javiermartinez_1998@hotmail.com>
2022-11-14 20:16:45 -0300

    variables2

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-let nombre = "javier" // string  
+//let nombre = "javier" // string  
 /*string=cadena de texto */
-let edad = 30 // number 
+//let edad = 30 // number 
 /* los numeros no necesitan comillas */
-let alumno = true; // booleans 
+//let alumno = true; // booleans 
 /* solo recibe dos tipos de datos true (verdadero) false (falso) */
 
-console.log (nombre) /*sirve para ingresar info en nuestro nav  */
-console.log (edad) /*lo q va entre parentecis el el nombre de la variable  */
-console.log (alumno) /*lo q va entre parentecis el el nombre de la variable  */
+//console.log (nombre) /*sirve para ingresar info en nuestro nav  */
+//console.log (edad) /*lo q va entre parentecis el el nombre de la variable  */
+//console.log (alumno) /*lo q va entre parentecis el el nombre de la variable  */
 
-const CURSO = "Js"
+//const CURSO = "Js"
 
-console.log (CURSO)
+//console.log (CURSO)
 
 //DIFERENCIAS ENTRE LET Y CONST 
+//LET puede estar declarada sin datos 
+//CONST debe tener su correspondiente dato
+
+//let nombre = "omar"
+//nombre = "jesus" //renombra o suplanta al primer nombre por eso no conviene usarlo
+//permite cambiar el dato
+
+//const apellido ="manias"
+//apellido ="otro" //me tira error porque no puedo suplantar el nombre a la variable 
+//no permite modificaciones
+
+//CONCATENACION- unir dos o mas datos 
+//let nombre ="jimena"
+//let apellido ="alcuaz"
+
+//console.log (nombre) 
+//console.log (apellido)
+
+//console.log (nombre+ "  " +apellido)
+
+//OPERADORES arimetricos
+/*
++ SUMA
+- RESTA
+*MULTIPLICACION
+/ DIVISION
+*/
+
+//let numero1 = 6
+//let numero2 = 3
+
+//let suma = numero1 + numero2
+//let multiplicacion = numero1 * numero2
+
+//console.log( numero1 + numero2)
+
+//console.log(multiplicacion) //declaro la variable que quiero q se imprima en pantalla
+
+//let nombre = "ingrese su nombre"
+
+//prompt ("ingresa tu nombre") // se usa para ingresar informacion
+
+let nombre = "coder" 
+alert (nombre) //se usa para mostras informacion
