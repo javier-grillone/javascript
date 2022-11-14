5cbb72f9044a6d09f797703b01e5f8ea05d0c98f
Javier Grillone <javiermartinez_1998@hotmail.com>
2022-11-14 19:01:49 -0300

    variables

---
+++ app.js
@@ -0,0 +1,16 @@
+let nombre = "javier" // string  
+/*string=cadena de texto */
+let edad = 30 // number 
+/* los numeros no necesitan comillas */
+let alumno = true; // booleans 
+/* solo recibe dos tipos de datos true (verdadero) false (falso) */
+
+console.log (nombre) /*sirve para ingresar info en nuestro nav  */
+console.log (edad) /*lo q va entre parentecis el el nombre de la variable  */
+console.log (alumno) /*lo q va entre parentecis el el nombre de la variable  */
+
+const CURSO = "Js"
+
+console.log (CURSO)
+
+//DIFERENCIAS ENTRE LET Y CONST 
